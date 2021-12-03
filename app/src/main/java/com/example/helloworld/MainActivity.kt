a76d79c16579a4de67021ea89187a071e145b045
unknown <Administrator@KANINI-LTP-469.gisbizdev.gisbiz.chennai.com>
2021-12-03 12:18:40 +0530

    Assignment Day 2 Name Count

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -3,26 +3,43 @@ package com.example.helloworld
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
 
+    var FinalCount = 0
+    var CountsMap = mutableMapOf<String,Int>("Somebody" to 0)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val ClickColorView= findViewById<TextView>(R.id.ClickCount)
-        val ClickRed = findViewById<TextView>(R.id.Red)
         val ClickGreen = findViewById<TextView>(R.id.Green)
-        val ClickBlue = findViewById<TextView>(R.id.Blue)
+        val UserNameENterView = findViewById<TextInputLayout>(R.id.nameText)
 
-        ClickRed.setOnClickListener{
-            ClickColorView.text = "Red was clicked"
-        }
+
+
         ClickGreen.setOnClickListener{
-            ClickColorView.text = "Green was clicked"
-        }
-        ClickBlue.setOnClickListener{
-            ClickColorView.text = "Blue was clicked"
+
+            val userName = UserNameENterView.editText?.text?.toString()
+            val maskedName =
+                if (userName.isNullOrEmpty()) "Somebody"
+                else userName
+
+            if(maskedName in CountsMap){
+                var count = CountsMap.get(maskedName)
+                if(count != null){
+                    count+=1
+                    CountsMap.put(maskedName,count)
+                    FinalCount = count
+                }
+            }
+            else{
+                CountsMap.put(maskedName,1)
+                FinalCount = 1
+            }
+
+            ClickColorView.text = "$maskedName was clicked $FinalCount times"
         }
     }
 
